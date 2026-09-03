cb84a100226d3e0c89ad4496e42bcd5bc93b20ee
dat-hua31 <dat.hua31@hcmut.edu.vn>
2026-09-03 15:47:25 +0700

    add initial logging

---
+++ src/core/logging.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <utility>
+#include <iostream>
+#include <print>
+#include <string_view>
+#include <format>
+#include <cassert>
+#include <exception>
+
+namespace centric::core
+{
+  enum class LogLevel {
+    Debug,
+    Info,
+    Warn,
+    Error,
+    Fatal
+  };
+
+  [[nodiscard]] constexpr std::string_view toString(LogLevel level) noexcept {
+    switch (level) {
+      case LogLevel::Debug: return "DEBUG";
+      case LogLevel::Info:  return "INFO";
+      case LogLevel::Warn:  return "WARN";
+      case LogLevel::Error: return "ERROR";
+      case LogLevel::Fatal: return "FATAL";
+    }
+    return "UNKNOWN";
+  }
+
+  template <typename... Args>
+  inline void log(LogLevel level, std::format_string<Args...> fmt, Args&&... args) {
+    auto& stream = (level == LogLevel::Error || level == LogLevel::Fatal) ? std::cerr : std::cout;
+    std::print(stream, "[{}] ", toString(level));
+    std::println(stream, fmt, std::forward<Args>(args)...);
+  }
+}
+
+#define CT_LOG(level, ...) do { centric::core::log(level,  __VA_ARGS__); } while(0)
+#define CT_DEBUG(...) CT_LOG(centric::core::LogLevel::Debug, __VA_ARGS__)
+#define CT_INFO(...) CT_LOG(centric::core::LogLevel::Info, __VA_ARGS__)
+#define CT_WARN(...) CT_LOG(centric::core::LogLevel::Warn, __VA_ARGS__)
+#define CT_ERROR(...) CT_LOG(centric::core::LogLevel::Error, __VA_ARGS__)
+#define CT_FATAL(...) do { CT_LOG(centric::core::LogLevel::Fatal, __VA_ARGS__); std::terminate(); } while(0)
+
+#define CT_ASSERT(condition) do { assert(condition); } while(0)
+
+
