9d641ccc1343392268e61c173573bcf79665a1e7
dat-hua31 <dat.hua31@hcmut.edu.vn>
2026-09-05 11:29:43 +0700

    add initial particle model

---
+++ src/particle/particle_system.cpp
@@ -0,0 +1,48 @@
+#include "particle/particle_system.hpp"
+
+#include <vector>
+#include <cstddef>
+
+namespace centric::particle
+{
+  void ParticleSystem::reserve(std::size_t capacity) {
+    mass.reserve(capacity);
+    position_x.reserve(capacity);
+    position_y.reserve(capacity);
+    position_z.reserve(capacity);
+    velocity_x.reserve(capacity);
+    velocity_y.reserve(capacity);
+    velocity_z.reserve(capacity);
+    color.reserve(capacity);
+  }
+
+  void ParticleSystem::resize(std::size_t new_size) {
+    mass.resize(new_size);
+    position_x.resize(new_size);
+    position_y.resize(new_size);
+    position_z.resize(new_size);
+    velocity_x.resize(new_size);
+    velocity_y.resize(new_size);
+    velocity_z.resize(new_size);
+    color.resize(new_size);
+  }
+
+  void ParticleSystem::clear() noexcept {
+    mass.clear();
+    position_x.clear();
+    position_y.clear();
+    position_z.clear();
+    velocity_x.clear();
+    velocity_y.clear();
+    velocity_z.clear();
+    color.clear();
+  }
+
+  std::size_t ParticleSystem::size() const noexcept {
+    return mass.size();
+  }
+
+  bool ParticleSystem::empty() const noexcept {
+    return mass.empty();
+  }
+}
---
+++ src/particle/particle_system.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "raylib.h"
+
+#include <vector>
+#include <cstddef>
+
+namespace centric::particle
+{
+  struct ParticleSystem {
+    std::vector<double> mass{};
+
+    std::vector<double> position_x{};
+    std::vector<double> position_y{};
+    std::vector<double> position_z{};
+
+    std::vector<double> velocity_x{};
+    std::vector<double> velocity_y{};
+    std::vector<double> velocity_z{};
+
+    std::vector<::Color> color{};
+
+    void reserve(std::size_t capacity);
+    void resize(std::size_t new_size);
+    void clear() noexcept;
+    [[nodiscard]] std::size_t size() const noexcept;
+    [[nodiscard]] bool empty() const noexcept;
+  };
+}
